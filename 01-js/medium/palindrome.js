a9954320c34437b25c0679e6e8c71e5801e5a8b8
iamvinalsharma <152777568+iamvinalsharma@users.noreply.github.com>
2023-12-08 17:09:48 +0530

    Completed palindrome.js
    https://gist.github.com/iamvinalsharma/9366c9cd1c93a7e4182599be04d08286

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,29 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
+// TESTS: https://github.com/100xdevs-cohort-2/assignments/blob/master/01-js/tests/palindrome.test.js
+
+// Pretty straight-forward program to understand.
+// Still have any questions? Please drop in your comments.
+
+function isPalindrome(testString) {
+  testString = testString.toLowerCase();
+  indexes = testString.length - 1;
+  let testString1 = "";
+  let testString2 = "";
+  for (let i = indexes; i >= 0 ; i--) {
+    if (testString[indexes - i].match(/[a-z]/i)) {
+      testString1 = testString1 + testString[indexes - i];
+    }
+    if (testString[i].match(/[a-z]/i)) {
+      testString2 = testString2 + testString[i];
+    }
+  }
+  if (testString1 == testString2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isPalindrome;
