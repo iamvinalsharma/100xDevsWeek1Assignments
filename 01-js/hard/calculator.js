e1dd994a855afeb15929a43d8277889588b10618
iamvinalsharma <152777568+iamvinalsharma@users.noreply.github.com>
2023-12-08 17:11:49 +0530

    Completed calculator.js
    https://gist.github.com/iamvinalsharma/50e1925ef5a1ea5d3c14dff40f1c7de3

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,5 +1,5 @@
 /*
-  Implement a class `Calculator` having below methods
+  Implement a class `Calculator` having below methods:
     - initialise a result variable in the constructor and keep updating it after every arithmetic operation
     - add: takes a number and adds it to the result
     - subtract: takes a number and subtracts it from the result
@@ -16,6 +16,57 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+// TESTS: https://github.com/100xdevs-cohort-2/assignments/blob/master/01-js/tests/calculator.test.js
+
+class Calculator {
+  
+  constructor () {
+    this.result = 0;
+  }
+
+  add (number) {
+    this.result = this.result + number;
+  }
+
+  subtract (number) {
+    this.result = this.result - number;
+  }
+
+  multiply (number) {
+    this.result = this.result * number;
+  }
+
+  divide (number) {
+    if (number == 0) {
+      throw new Error();
+    } else {
+      this.result = this.result / number;
+    }
+  }
+
+  clear () {
+    this.result = 0;
+  }
+
+  getResult () {
+    return this.result;
+  }
+
+  calculate (expression) {
+    let updatedExpression = "";
+    for (let i = 0; i < expression.length; i++) {
+      if (expression[i].match(/[a-z]/i)) {
+        throw new Error();
+      }
+      if (expression[i] != " ") {
+        updatedExpression = updatedExpression + expression[i];
+      }
+    }
+    if (updatedExpression.includes("/0")) {
+      throw new Error();
+    }
+    this.result = eval(expression);
+  }
+}
 
 module.exports = Calculator;
