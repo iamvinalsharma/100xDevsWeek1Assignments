21a19769c7ee0d142ecda695f8b38ee227ddd2f9
iamvinalsharma <152777568+iamvinalsharma@users.noreply.github.com>
2023-12-08 17:11:16 +0530

    Completed todo-list.js
    https://gist.github.com/iamvinalsharma/a5fbed99b588c0f0485b3434f854149f

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -10,7 +10,43 @@
   Once you've implemented the logic, test your code by running
 */
 
+// TESTS: https://github.com/100xdevs-cohort-2/assignments/blob/master/01-js/tests/todo-list.test.js
+
 class Todo {
+
+  constructor() {
+    this.list_of_todos = [];
+  }
+
+  add(todo) {
+    this.list_of_todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    this.list_of_todos.splice(indexOfTodo, 1);
+  }
+
+  update(index, updatedTodo) {
+    if (this.list_of_todos.length > index ) {
+      this.list_of_todos.splice(index, 1, updatedTodo);
+    }
+  }
+
+  getAll() {
+    return this.list_of_todos;
+  }
+
+  get(indexOfTodo) {
+    if (this.list_of_todos.length > indexOfTodo ) {
+      return this.list_of_todos[indexOfTodo];
+    } else {
+      return null;
+    }   
+  }
+
+  clear() {
+    this.list_of_todos = [];
+  }
 
 }
 
